7c3cf735b707973628396a69e4752ce8603c6193
Zenx007 <zenxpokas@gmail.com>
2025-03-23 22:48:24 -0300

    Update

---
+++ src/main/java/com/planner/project/trip/TripController.java
@@ -0,0 +1,9 @@
+package com.planner.project.trip;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trips")
+public class TripController {
+}
