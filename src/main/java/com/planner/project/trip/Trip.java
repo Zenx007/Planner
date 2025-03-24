6f60b3b6ada6c785af06bf95fbe584ffa2a97426
Zenx007 <zenxpokas@gmail.com>
2025-03-23 23:36:04 -0300

    Update

--- src/main/java/com/planner/project/trip/Trip.java
+++ src/main/java/com/planner/project/trip/Trip.java
@@ -7,9 +7,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Entity
+@Table(name = "trips")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -33,8 +35,19 @@ public class Trip {
     private Boolean isConfirmed;
 
     @Column(name = "owner_name", nullable = false)
-    private String OwnerName;
+    private String ownerName;
 
     @Column (name = "owner_email", nullable = false)
-    private String OwnerEmail;
+    private String ownerEmail;
+
+    public Trip(TripRequestPayload data) {
+        this.destination = data.destination();
+        this.isConfirmed = false;
+        this.ownerEmail = data.owner_email();
+        this.ownerName = data.owner_name();
+        this.startsAt = LocalDateTime.parse(data.starts_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), DateTimeFormatter.ISO_DATE_TIME);
+
+
+    }
 }
