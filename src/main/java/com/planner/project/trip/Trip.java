a6b15f0b575525ad1c68d06afa8acebf79baf696
Zenx007 <zenxpokas@gmail.com>
2025-03-23 23:01:19 -0300

    Update

--- src/main/java/com/planner/project/trip/Trip.java
+++ src/main/java/com/planner/project/trip/Trip.java
@@ -1,11 +1,20 @@
 package com.planner.project.trip;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Trip {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
