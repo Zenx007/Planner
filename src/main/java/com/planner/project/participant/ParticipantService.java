02d694a936abe6da7389dc2d85482f78b6be7ad9
Zenx007 <zenxpokas@gmail.com>
2025-03-23 23:43:59 -0300

    Update

--- src/main/java/com/planner/project/participant/ParticipantService.java
+++ src/main/java/com/planner/project/participant/ParticipantService.java
@@ -1,8 +1,11 @@
 package com.planner.project.participant;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class ParticipantService {
 
     public void registerParticipantsToEvent(List<String> participantsToInvite, UUID id) {
