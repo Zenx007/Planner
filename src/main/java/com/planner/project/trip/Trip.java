922dafca9f24283ab6bd3c676d403a07c0de9238
Zenx007 <zenxpokas@gmail.com>
2025-03-23 22:18:02 -0300

    Update

--- src/main/java/com/planner/project/trip/Trip.java
+++ src/main/java/com/planner/project/trip/Trip.java
@@ -11,8 +11,21 @@ public class Trip {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @Column(nullable = false)
     private String destination;
 
     @Column(name = "starts_at", nullable = false)
     private LocalDateTime startsAt;
+
+    @Column (name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column (name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String OwnerName;
+
+    @Column (name = "owner_email", nullable = false)
+    private String OwnerEmail;
 }
