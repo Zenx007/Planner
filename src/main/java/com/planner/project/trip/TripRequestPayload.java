a3360d7976a337d4493c87d2fa231f41bb3fd082
Zenx007 <zenxpokas@gmail.com>
2025-03-23 23:41:19 -0300

    Update

--- src/main/java/com/planner/project/trip/TripRequestPayload.java
+++ src/main/java/com/planner/project/trip/TripRequestPayload.java
@@ -4,5 +4,5 @@ import org.apache.tomcat.util.codec.binary.StringUtils;
 
 import java.util.List;
 
-public record TripRequestPayload (String Destination, String starts_at, String ends_at, List<String> emails_to_invite, String owner_email, String owner_name){
+public record TripRequestPayload (String destination, String starts_at, String ends_at, List<String> emails_to_invite, String owner_email, String owner_name){
 }
