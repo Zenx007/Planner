a9aa02cc114f3277b12c36490f66dd24c507a703
Zenx007 <zenxpokas@gmail.com>
2025-03-25 23:59:58 -0300

    Update

---
+++ src/main/java/com/planner/project/participant/Participant.java
@@ -0,0 +1,9 @@
+package com.planner.project.participant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "participants")
+public class Participant {
+}
