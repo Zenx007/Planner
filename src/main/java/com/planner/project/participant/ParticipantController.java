609f46efa222644d8077cd1e265b691a46a2caaa
Zenx007 <zenxpokas@gmail.com>
2025-03-27 23:56:40 -0300

    Update

--- src/main/java/com/planner/project/participant/ParticipantController.java
+++ src/main/java/com/planner/project/participant/ParticipantController.java
@@ -1,4 +1,14 @@
 package com.planner.project.participant;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/participants")
 public class ParticipantController {
+
+    @PostMapping("/{id}/confirm")
+    public ResponseEntity<Participant> confirmParticipant(){}
 }
