b15c51ede1a951e260088591c300c0b39ce3114c
Zenx007 <zenxpokas@gmail.com>
2025-03-27 23:58:58 -0300

    Update

--- src/main/java/com/planner/project/participant/ParticipantController.java
+++ src/main/java/com/planner/project/participant/ParticipantController.java
@@ -1,14 +1,17 @@
 package com.planner.project.participant;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/participants")
 public class ParticipantController {
 
     @PostMapping("/{id}/confirm")
-    public ResponseEntity<Participant> confirmParticipant(){}
+    public ResponseEntity<Participant> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload){
+        Optional<Participant> participant = this.repository.findById(id);
+    }
 }
