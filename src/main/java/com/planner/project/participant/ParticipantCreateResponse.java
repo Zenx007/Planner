283af51cca0e9cc07c376180379ab1ffe779f383
Zenx007 <zenxpokas@gmail.com>
2025-03-29 13:12:47 -0300

    Update

--- src/main/java/com/planner/project/participant/ParticipantCreateResponse.java
+++ src/main/java/com/planner/project/participant/ParticipantCreateResponse.java
@@ -2,5 +2,5 @@ package com.planner.project.participant;
 
 import java.util.UUID;
 
-public record ParticipantCreateResponse(UUID id, String name, String email, Boolean isConfirmed) {
+public record ParticipantCreateResponse(UUID id) {
 }
