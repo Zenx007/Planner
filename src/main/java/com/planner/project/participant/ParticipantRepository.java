92169cc4a354e7f617954241827a60b7adb65cf4
Zenx007 <zenxpokas@gmail.com>
2025-03-29 13:12:58 -0300

    Update

--- src/main/java/com/planner/project/participant/ParticipantRepository.java
+++ src/main/java/com/planner/project/participant/ParticipantRepository.java
@@ -2,9 +2,12 @@ package com.planner.project.participant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+
+    List<Participant> findByTripId(UUID tripId);
 
 
 }
