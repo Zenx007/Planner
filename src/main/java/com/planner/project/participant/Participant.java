2a0f855b6cc28802ec73f05e1ba8e501800f5a2d
Zenx007 <zenxpokas@gmail.com>
2025-03-29 13:04:20 -0300

    Update

--- src/main/java/com/planner/project/participant/Participant.java
+++ src/main/java/com/planner/project/participant/Participant.java
@@ -26,6 +26,13 @@ public class Participant {
     @JoinColumn(name= "trip_id", nullable = false)
     private Trip trip;
 
+    public Participant(String email, Trip trip){
+        this.email = email;
+        this.trip = trip;
+        this.isConfirmed = false;
+        this.name = "";
+    }
+
 
 
 }
