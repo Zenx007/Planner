6f332bd6b2b006eff9d7015d84a53712750ef9ac
Zenx007 <zenxpokas@gmail.com>
2025-03-30 20:41:35 -0300

    Update

--- src/main/java/com/planner/project/trip/TripController.java
+++ src/main/java/com/planner/project/trip/TripController.java
@@ -75,4 +75,24 @@ public class TripController {
         return ResponseEntity.notFound().build();
 
     }
+
+    @PostMapping("/{id}/invite")
+    public ResponseEntity<ParticipantCreateResponse> inviteParticipant(@PathVariable UUID id; @RequestBody
+    ParticipantRequestPayload payload) {
+
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if(trip.isPresent()){
+            Trip rawTrip = trip.get();
+
+
+            ParticipantCreateResponse participantResponse  = this.participantService.registerParticipantToEvent(payload.email(), rawTrip);
+
+            if(rawTrip.getIsConfirmed()) this.participantService.triggerConfirmationEmailToParticipant(payload.email());
+
+            return ResponseEntity.ok(participantResponse);
+        }
+        return ResponseEntity.notFound().build();
+
+    }
 }
