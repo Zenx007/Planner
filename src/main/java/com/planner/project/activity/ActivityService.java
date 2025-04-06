8903903240aded4ea4a5e218287016b0c0afbf96
Zenx007 <zenxpokas@gmail.com>
2025-04-06 17:46:19 -0300

    Update

--- src/main/java/com/planner/project/activity/ActivityService.java
+++ src/main/java/com/planner/project/activity/ActivityService.java
@@ -1,7 +1,20 @@
 package com.planner.project.activity;
 
+import com.planner.project.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActivityService {
+
+    @Autowired
+    private ActivityRepository repository;
+
+    public ActivityResponse registerActivity(ActivityRequestPayload payload, Trip trip) {
+        Activity newActivity = new Activity(payload.title(), payload.occurs_at(), trip);
+
+        this.repository.save(newActivity);
+
+        return new ActivityResponse(newActivity.getId());
+    }
 }
