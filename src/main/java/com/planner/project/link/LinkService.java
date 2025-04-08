ae54198a1b2f77a7024fad96265e8dcef9b70380
Zenx007 <zenxpokas@gmail.com>
2025-04-07 23:54:49 -0300

    Update

---
+++ src/main/java/com/planner/project/link/LinkService.java
@@ -0,0 +1,20 @@
+package com.planner.project.link;
+
+import com.planner.project.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LinkService {
+
+    @Autowired
+    private LinkRepository repository;
+
+    public LinkResponse registerLink (LinkRequestPayload payload, Trip trip) {
+        Link newLink = new Link(payload.title(),payload.url(),trip);
+
+        this.repository.save(newLink);
+
+        return new LinkResponse (newLink.getId());
+    }
+}
