d21924bdbaf182d690e7be76e1cdc89f6b84ccbf
Zenx007 <zenxpokas@gmail.com>
2025-04-07 23:56:08 -0300

    Update

--- src/main/java/com/planner/project/link/LinkService.java
+++ src/main/java/com/planner/project/link/LinkService.java
@@ -4,6 +4,9 @@ import com.planner.project.trip.Trip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class LinkService {
 
@@ -16,5 +19,10 @@ public class LinkService {
         this.repository.save(newLink);
 
         return new LinkResponse (newLink.getId());
+    }
+
+    public List<LinkData> getAllLinksFromTrip (UUID tripId) {
+
+        return this.repository.findByTripId(tripId).stream().map(link -> new LinkData(link.getId(), link.getTitle(), link.getUrl())).toList();
     }
 }
